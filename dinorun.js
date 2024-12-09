683205e6ddf99c3cc0077bc9c05c97289c2539c3
Jianyi (Birdy) Lu <153916948+BirdyLu@users.noreply.github.com>
2024-12-09 13:40:31 +0800

    Listen to DOMContentLoaded

--- dinorun.js
+++ dinorun.js
@@ -1,42 +1,45 @@
-var dinosaur = document.getElementById('dinosaur');
-
-var mouseX = mouseX;
-var rect = dinosaur.getBoundingClientRect();
-var petX = rect.left;
-
-// For tracking mouse movement
-function trackMouse(event) {
-  mouseX = event.clientX;
-}
-document.addEventListener('mousemove', trackMouse);
-
-function updatePosition() {
-  var dx = mouseX - petX;
-  const error = 2;
-
-  if (Math.abs(dx) >= error) {
-    if (mouseX > petX) {
-      dinosaur.style.backgroundImage = `url('/dino/dino-run-right.gif')`;
-      petX += error;
-      dinosaur.style.left = petX + 'px';
-    } 
-    else if (mouseX < petX) {
-      dinosaur.style.backgroundImage = `url('/dino/dino-run-left.gif')`;
-      petX -= error;
-      dinosaur.style.left = petX + 'px';
+document.addEventListener('DOMContentLoaded', () => {
+  
+  var dinosaur = document.getElementById('dinosaur');
+  
+  var mouseX = mouseX;
+  var rect = dinosaur.getBoundingClientRect();
+  var petX = rect.left;
+  
+  // For tracking mouse movement
+  function trackMouse(event) {
+    mouseX = event.clientX;
+  }
+  document.addEventListener('mousemove', trackMouse);
+  
+  function updatePosition() {
+    var dx = mouseX - petX;
+    const error = 2;
+  
+    if (Math.abs(dx) >= error) {
+      if (mouseX > petX) {
+        dinosaur.style.backgroundImage = `url('/dino/dino-run-right.gif')`;
+        petX += error;
+        dinosaur.style.left = petX + 'px';
+      } 
+      else if (mouseX < petX) {
+        dinosaur.style.backgroundImage = `url('/dino/dino-run-left.gif')`;
+        petX -= error;
+        dinosaur.style.left = petX + 'px';
+      }
+    } else {
+      dinosaur.style.backgroundImage = `url('/dino/dino_standing.webp')`;
     }
-  } else {
-    dinosaur.style.backgroundImage = `url('/dino/dino_standing.webp')`;
+    
+    requestAnimationFrame(updatePosition);
   }
   
-  requestAnimationFrame(updatePosition);
-}
+  
+  // petX = window.innerWidth / 2;
+  // dinosaur.style.left = petX + 'px';
+  
+  updatePosition()
+  
+  }
 
-
-// petX = window.innerWidth / 2;
-// dinosaur.style.left = petX + 'px';
-
-updatePosition()
-
-}
-
+)};
